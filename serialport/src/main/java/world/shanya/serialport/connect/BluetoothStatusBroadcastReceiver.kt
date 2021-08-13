6492d3522729ba998becc751f8b22256e7cd1935
Shanyaliux <445502929@qq.com>
2021-08-13 12:30:02 +0800

    2021-8-13 update
    - 修复内置搜索页面搜索到设备名为空的设备造成闪退的问题
    - 修复连接设备成功后会报广播接收器没有被注册的问题
    - 优化搜索结果的处理
    - 内置搜索页面新增设备类型字段
    - 内置搜索页面设备图标跟设备类型相匹配
    - 新增附带搜索设备类型的搜索状态回调
    - 新增通过设置地址自动区分设备连接函数
    - 优化对蓝牙开关状态的处理

---
+++ serialport/src/main/java/world/shanya/serialport/connect/BluetoothStatusBroadcastReceiver.kt
@@ -0,0 +1,51 @@
+package world.shanya.serialport.connect
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import world.shanya.serialport.SerialPort
+import world.shanya.serialport.strings.SerialPortToast
+import world.shanya.serialport.strings.SerialPortToastBean
+import world.shanya.serialport.tools.LogUtil
+import world.shanya.serialport.tools.ToastUtil
+
+/**
+ * BluetoothStatusBroadcastReceiver 蓝牙连接状态变更广播接收器
+ * 在这里只对传统蓝牙的断开进行处理
+ * 实时监听蓝牙断开，若蓝牙断开则自动打开
+ * @Author Shanya
+ * @Date 2021-7-21
+ * @Version 4.0.0
+ */
+class BluetoothStatusBroadcastReceiver:BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
+                SerialPortConnect.connectedLegacyDevice?.let {
+                    SerialPort._legacyDisconnect()
+                }
+            }
+
+            BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, 0)) {
+                    BluetoothAdapter.STATE_TURNING_ON -> {
+                        LogUtil.log("蓝牙打开成功")
+                        context?.let {
+                            ToastUtil.toast(it, SerialPortToast.openBluetoothSucceeded)
+                        }
+                    }
+                    BluetoothAdapter.STATE_TURNING_OFF -> {
+                        if (!SerialPort.bluetoothAdapter.enable()) {
+                            LogUtil.log("蓝牙打开失败")
+                            context?.let {
+                                ToastUtil.toast(it, SerialPortToast.openBluetoothFailed)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
